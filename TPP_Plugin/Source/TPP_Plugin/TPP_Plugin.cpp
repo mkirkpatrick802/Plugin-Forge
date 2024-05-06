d598746d360e5ac30aca90daa32f274f71ca05f4
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2024-05-05 21:54:14 -0400

    Clean Up

--- TPP_Plugin/Source/TPP_Plugin/TPP_Plugin.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TPP_Plugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPP_Plugin, "TPP_Plugin" );
